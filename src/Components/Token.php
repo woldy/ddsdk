110374cb7f7308d8d10ba9591a714b0849c1b037
woldy <king@woldy.net>
2016-05-09 16:54:35 +0800

    Signed-off-by: woldy <king@woldy.net>

--- src/Components/Token.php
+++ src/Components/Token.php
@@ -38,39 +38,81 @@ class token{
             	exit;
         	}
             $accessToken = $response->body->access_token;
-            Cache::put('corp_access_token', $accessToken,120);
+            Cache::put('corp_access_token', $accessToken,60);
         }
         return $accessToken;
 	}
 
 	public function getSsoToken(){
-
 		//https://oapi.dingtalk.com/sso/gettoken?corpid=id&corpsecret=ssosecret
 	}
 
 
 	public function getJsapiTicket(){
+        $jsticket = Cache::get('js_ticket');
+        $param=http_build_query(
+        	array(
+        		'type' =>'jsapi', 
+        		'access_token'=>$this->getAccessToken()
+        	)
+        );
+        if (!$jsticket)
+        {
+            $response = Request::get('https://oapi.dingtalk.com/get_jsapi_ticket?'.$param)->send();
+            if ($response->hasErrors()){
+            	var_dump($response);
+            	exit;
+        	}
+        	if ($response->body->errcode != 0){
+            	var_dump($response->body);
+            	exit;
+        	}
+            $jsticket = $response->body->ticket;
+            Cache::put('js_ticket',$jsticket,60);
+        }
+        return $jsticket;
 		//https://oapi.dingtalk.com/get_jsapi_ticket?access_token=ACCESS_TOKE
 	}
 
-	public function getSignature(){
-		
+	public function getSignature($ticket, $nonceStr, $timeStamp, $url){
+        $plain = 'jsapi_ticket=' . $ticket .
+            '&noncestr=' . $nonceStr .
+            '&timestamp=' . $timeStamp .
+            '&url=' . $url;
+        return sha1($plain);		
 	}
 
-	public static function getJsConfig(){
+
+    private function getCurPageURL(){
+        $pageURL = 'http';
+        if (array_key_exists('HTTPS',$_SERVER)&&$_SERVER["HTTPS"] == "on"){
+            $pageURL .= "s";
+        }
+        $pageURL .= "://";
+        if ($_SERVER["SERVER_PORT"] != "80"){
+            $pageURL .= $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
+        }
+        else{
+            $pageURL .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+        }
+        return $pageURL;
+    }
+
+
+	public function getJsConfig(){
         $corpId =$this->CorpID;
         $agentId = $this->AgentID;
         $timeStamp = time();
-        $nonceStr = md5($timestamp.'woldy');
+        $nonceStr = md5($timeStamp.'woldy');
         
-        $url = self::curPageURL();
-        $corpAccessToken = self::getAccessToken();
+        $url = $this->getCurPageURL();
+        $corpAccessToken = $this->getAccessToken();
         if (!$corpAccessToken)
         {
             Log::e("[getConfig] ERR: no corp access token");
         }
-        $ticket = self::getTicket($corpAccessToken);
-        $signature = self::sign($ticket, $nonceStr, $timeStamp, $url);
+        $ticket = $this->getJsapiTicket($corpAccessToken);
+        $signature = $this->getSignature($ticket, $nonceStr, $timeStamp, $url);
         
         $config = array(
             'url' => $url,
@@ -78,15 +120,9 @@ class token{
             'agentId' => $agentId,
             'timeStamp' => $timeStamp,
             'corpId' => $corpId,
-            'signature' => $signature);
+            'signature' => $signature
+        );
         return json_encode($config, JSON_UNESCAPED_SLASHES);
 	}
 
-	public static function sign($ticket, $nonceStr, $timeStamp, $url){
-        $plain = 'jsapi_ticket=' . $ticket .
-            '&noncestr=' . $nonceStr .
-            '&timestamp=' . $timeStamp .
-            '&url=' . $url;
-        return sha1($plain);
-    }
 }
