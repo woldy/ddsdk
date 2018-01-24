0ff52c8380857300c25d466c1f3afb72106cd192
woldy-tal <king@woldy.net>
2018-01-24 11:30:14 +0800

    modified:   src/Components/Util.php

--- src/Components/Util.php
+++ src/Components/Util.php
@@ -29,7 +29,7 @@ class Util {
       //     $response->body=json_decode($response->body);
       // }
 
-      if ($response->body->errcode == 90002){
+      if (!empty($response->body) && $response->body->errcode == 90002){
         echo "好像超限了，60秒后重试".$response->uri."\n";
         sleep(60);
         return self::try_http_query($response,3,$exit,$url);
